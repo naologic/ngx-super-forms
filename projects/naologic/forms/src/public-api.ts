58035b85a12c59f7e9999ba5921b02aba01694f3
Florin <galanflorind@gmail.com>
2021-10-12 20:46:49 +0300

    Removing nao mask

--- projects/naologic/forms/src/public-api.ts
+++ projects/naologic/forms/src/public-api.ts
@@ -12,14 +12,3 @@ export * from './lib/nao-form-control.class';
 export * from './lib/nao-form-array.class';
 export * from './lib/nao-form-builder.class';
 export * from './lib/nao-validators';
-
-/*
- * Public API Surface of Nao mask
- */
-export * from './lib/nao-mask/nao-mask.module';
-export * from './lib/nao-mask/mask.directive';
-export * from './lib/nao-mask/mask.pipe';
-export * from './lib/nao-mask/mask.service';
-export * from './lib/nao-mask/mask-applier.service';
-export * from './lib/nao-mask/config';
-export * from './lib/nao-mask/custom-keyboard-event';
